b4fab734f374b8978f29dd40e69ad953b0020843
Trieu Duong <luongtrieuduongdz@gmail.com>
2025-08-06 00:32:36 +0700

    commit

--- NHOM1_DA1_QUANLINHANSU/GUI/UC/UC_TrangChu.Designer.cs
+++ NHOM1_DA1_QUANLINHANSU/GUI/UC/UC_TrangChu.Designer.cs
@@ -29,29 +29,20 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UC_TrangChu));
-            pictureBox1 = new PictureBox();
             label1 = new Label();
             label2 = new Label();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(0, 118);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(992, 469);
-            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
             // 
             // label1
             // 
             label1.Anchor = AnchorStyles.Top;
             label1.AutoSize = true;
             label1.Font = new Font("Times New Roman", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(86, 49);
+            label1.Location = new Point(86, 36);
             label1.Name = "label1";
             label1.Size = new Size(806, 42);
             label1.TabIndex = 1;
@@ -62,30 +53,51 @@
             label2.Anchor = AnchorStyles.Top;
             label2.AutoSize = true;
             label2.Font = new Font("Times New Roman", 13.8F);
-            label2.Location = new Point(109, 106);
+            label2.Location = new Point(111, 98);
             label2.Name = "label2";
             label2.Size = new Size(747, 26);
             label2.TabIndex = 2;
             label2.Text = "  Khám phá những tiện ích hỗ trợ quản lí nhân sự tăng hiệu suất giảm nhân lực ";
             // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(0, 127);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(992, 460);
+            panel1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(986, 454);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
             // UC_TrangChu
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(panel1);
             Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(pictureBox1);
             Name = "UC_TrangChu";
             Size = new Size(992, 587);
+            panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private PictureBox pictureBox1;
         private Label label1;
         private Label label2;
+        private Panel panel1;
+        private PictureBox pictureBox1;
     }
 }
