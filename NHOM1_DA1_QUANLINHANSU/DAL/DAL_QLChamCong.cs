329237a5a9f504665dff3ca25438a4961d6c8455
Trieu Duong <luongtrieuduongdz@gmail.com>
2025-08-08 00:06:29 +0700

    UPDATE

--- NHOM1_DA1_QUANLINHANSU/DAL/DAL_QLChamCong.cs
+++ NHOM1_DA1_QUANLINHANSU/DAL/DAL_QLChamCong.cs
@@ -1,9 +1,38 @@
 ﻿using NHOM1_DA1_QUANLINHANSU.Models;
+using NHOM1_DA1_QUANLINHANSU.DTO;
+using Microsoft.EntityFrameworkCore;
 
 namespace NHOM1_DA1_QUANLINHANSU.DAL
 {
     internal class DAL_QLChamCong
     {
-       
+       private Nhom1Da1QlnsContext _db = new Nhom1Da1QlnsContext();
+
+        public List<DTO_QLChamCong> GetAllChamCong()
+        {
+           return _db.Chamcongs.Include(nv => nv.IdnvNavigation)
+                .Include(lc => lc.IdloaiCongNavigation).ToList()
+                .Select((x, index) => new DTO_QLChamCong
+                {
+                    STT = index + 1,
+                    IdchamCong = x.IdchamCong,
+                    Nam = x.Nam,
+                    Thang = x.Thang,
+                    Ngay = x.Ngay,
+                    GioVao = x.GioVao,
+                    PhutVao = x.PhutVao,
+                    GioRa = x.GioRa,
+                    PhutRa = x.PhutRa,
+                    CaLam = x.CaLam,
+                    TenLoaiCong = x.IdloaiCongNavigation.TenLoaiCong,
+                    Idnv = x.Idnv,
+                    TenNv = x.IdnvNavigation.TenNv,
+                    GioiTinh = x.IdnvNavigation.GioiTinh,
+                    NgaySinh = x.IdnvNavigation.NgaySinh,
+                    Sdt = x.IdnvNavigation.Sdt,
+                    Cccd = x.IdnvNavigation.Cccd,
+                    DiaChi = x.IdnvNavigation.DiaChi
+                }).ToList();
+        }
     }
 }
--- NHOM1_DA1_QUANLINHANSU/DTO/DTO_QLChamCong.cs
+++ NHOM1_DA1_QUANLINHANSU/DTO/DTO_QLChamCong.cs
@@ -1,9 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace NHOM1_DA1_QUANLINHANSU.Models
+namespace NHOM1_DA1_QUANLINHANSU.DTO
 {
-    public class ChamCong
+    internal class DTO_QLChamCong
     {
-       
+        public int STT { get; set; }
+
+        [DisplayName("ID chấm công")]
+        public int IdchamCong { get; set; }
+
+        [DisplayName("Năm")]
+        public int Nam { get; set; }
+
+        [DisplayName("Tháng")]
+        public int Thang { get; set; }
+
+        [DisplayName("Ngày")]
+        public int Ngay { get; set; }
+
+        [DisplayName("Giờ vào")]
+        public int GioVao { get; set; }
+
+        [DisplayName("Phút vào")]
+        public int PhutVao { get; set; }
+
+        [DisplayName("Giờ ra")]
+        public int GioRa { get; set; }
+
+        [DisplayName("Phút ra")]
+        public int PhutRa { get; set; }
+
+        [DisplayName("Ca làm")]
+        public string CaLam { get; set; } = null!;
+
+        [DisplayName("Tên loại công")]
+        public string TenLoaiCong { get; set; } = null!;
+
+        [DisplayName("ID nhân viên")]
+        public int Idnv { get; set; }
+
+        [DisplayName("Tên nhân viên")]
+        public string TenNv { get; set; } = null!;
+
+        [DisplayName("giới tính")]
+        public string GioiTinh { get; set; } = null!;
+
+        [DisplayName("Ngày sinh")]
+        public DateOnly NgaySinh { get; set; }
+
+        [DisplayName("Số điện thoại")]
+        public string Sdt { get; set; } = null!;
+
+        [DisplayName("CCCD")]
+        public string Cccd { get; set; } = null!;
+
+        [DisplayName("Địa chỉ")]
+        public string DiaChi { get; set; } = null!;
+
+        public byte[] HinhAnh { get; set; } = null!;
     }
 }
